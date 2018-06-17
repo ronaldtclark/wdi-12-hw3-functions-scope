6b33ee17e371ea074c86dd8d0d217c4a01d9463b
Ron Clark <ronaldtclark@gmail.com>
2018-06-17 04:06:48 -0500

    Commit 2 - Palindrome

--- w1d4_homework-no-git/js/app.js
+++ w1d4_homework-no-git/js/app.js
@@ -3,11 +3,21 @@
 // 2. A parameter is the variable within the function as part of its definition. An argument is the value processed by the function when it's invoked.
 // 3. Return will give you an actual value produced by the function, an exit value, and stops the function. Console.log is used by developers to "print" something that has set-up parameters, primarily to check for bugs or define something.
 
+// PALINDROME
 
-
-
+const checkPalindrome = (word) => {
+  let reverseWord = word.split(" ").reverse().join(" ");
+  if (word === reverseWord) {
+    return true;
+  } else if (word != reverseWord) {
+    return false;
+  }
   
+};
 
+console.log(checkPalindrome ("radar"));
+console.log(checkPalindrome ("mouse"));
+console.log(checkPalindrome ("wooooork"));
 
 
 
