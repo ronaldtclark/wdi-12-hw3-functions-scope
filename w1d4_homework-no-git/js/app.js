b44880f46774abc94c63faf56c6028345fa79747
Ron Clark <ronaldtclark@gmail.com>
2018-06-17 03:37:40 -0500

    Commit 1 - Verbal Questions

---
+++ w1d4_homework-no-git/js/app.js
@@ -0,0 +1,16 @@
+// VERBAL QUESTIONS
+
+// 2. A parameter is the variable within the function as part of its definition. An argument is the value processed by the function when it's invoked.
+// 3. Return will give you an actual value produced by the function, an exit value, and stops the function. Console.log is used by developers to "print" something that has set-up parameters, primarily to check for bugs or define something.
+
+
+
+
+  
+
+
+
+
+
+
+
